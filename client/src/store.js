71838c9f932633c611e399f7af5a4dae1b3d4881
Zimam Sayeed <zmmsayeed@gmail.com>
2019-07-18 21:18:49 +0530

    latest changes

---
+++ client/src/store.js
@@ -0,0 +1,15 @@
+// kinda like an entry point to redux store
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(rootReducer, initialState, compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+export default store;
